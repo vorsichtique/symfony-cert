8072c84a574701ccd4d2ab063d603fcd47c23129
Malu <malu@example.com>
2017-08-16 09:25:58 +0200

    Options resolver comp example

--- components/web/index.php
+++ components/web/index.php
@@ -5,11 +5,12 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->get('/', function() use($app) {
-    return 'Hello ';
+    return '<a href="options-resolver">Options Resolver Component</a>';
 });
 
-$app->get('/hello/{name}', function($name) use($app) {
-    return 'Hello '.$app->escape($name);
+$app->get('/options-resolver', function() use($app) {
+    $expl = new Malu\OptionsResolver\Explanation();
+    return 'options-resolver ';
 });
 
 $app->run();
